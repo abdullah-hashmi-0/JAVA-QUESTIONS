a62922fc8cc046f2edb933135847ddb8b7c02f88
Abdullah Hashmi <abdhashmi333@gmail.com>
2025-11-26 00:12:44 +0530

    Remove Pattern and Student classes; add BuilderString2, ConvertToUpperCase, and LargestString classes with implementations

---
+++ JAVA/BuilderString2.java
@@ -0,0 +1,11 @@
+public class BuilderString2 {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("");
+
+        for(char ch = 'a'; ch <= 'z'; ch++) {
+            sb.append(ch);
+        }
+
+        System.out.println(sb + "\n" + "Length is:" + sb.length());
+    }
+}
